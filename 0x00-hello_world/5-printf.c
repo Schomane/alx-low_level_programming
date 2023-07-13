fd1e069919476a9d9c4233aa013719601fc23ecc
schomane <skhumbuzoschomane.com>
2023-07-13 15:57:02 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * main- entry point
- * Return: 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-return:(0);
+    	return (0);
 }
