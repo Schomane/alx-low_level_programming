1970b705f7b59bf74e05d3d2a55d82434aa0f2c7
schomane <skhumbuzoschomane.com>
2023-07-13 15:22:02 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
-* main - A program that print a line using the project function 
-* Return: 0 (Success)
-*/
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0)
-}
+ * main - "with proper grammar, but the outcome is a piece of art," followed newline
+ * Return: always 0.
+ *
+ * /
+ 
+ int main(void)
+ {
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+ }
