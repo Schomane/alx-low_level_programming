84bc7f0b6e5a610260dde8903afa36c7730dee62
schomane <skhumbuzoschomane.com>
2023-07-13 15:45:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <studio.h>
 /**
- * main - entry point
- * return: always 0(success)
- * */
+ * main- entry point
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	puts(*\*"Programming is like building a multilingual puzzle");
-return: (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return:(0);
 }
