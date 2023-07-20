5fe59b161c6a1d6d7b22693d20192f9b467eec28
schomane <skhumbuzoschomane.com>
2023-07-20 11:08:13 -0700

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,25 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
- *
- * Return: 0 or 1
+ * main - causes an infinite loop
+ * Return: 0
  */
 
-int _isdigit(int c)
+int main(void)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+ int i;
+
+ printf("Infinite loop incoming :(\n");
+
+ i = 0;
+
+ /*while (i < 10)*/
+ /*{*/
+ /* putchar(i);*/
+ /*}*/
+
+ printf("Infinite loop avoided! \\o/\n");
+
+ return (0);
 }
+
