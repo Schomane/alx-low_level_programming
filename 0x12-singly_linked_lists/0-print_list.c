b7cadc86a0d5cfe10a26df3d729703e74f62792f
schomane <skhumbuzoschomane@gmail.com>
2023-08-24 14:47:27 -0700

    0 print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
 * print_list - prints all the elements of a linked list
 * @h: pointer to the list_t list to print
@@ -21,4 +20,3 @@ s++;
 }
 return (s);
 }
-
